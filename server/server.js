69e1acda700f0328d062040c87f9446f292c993d
spoonman <Chenfmmu@163.com>
2017-12-09 08:52:28 +0800

    with some change

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const path = require("path");
+const publicPath = path.join(__dirname, "/../public")
+const port = process.env.PORT || 3000
+const express = require("express");
+
+const app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+    console.log(`Started on port ${port}`);
+});
+
+module.exports = {app}
