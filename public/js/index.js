ecc2c6b7dd79818382b15d2158bbbb9f0256d9fe
spoonman <Chenfmmu@163.com>
2017-12-14 19:41:29 +0800

    Add timestamp

--- public/js/index.js
+++ public/js/index.js
@@ -13,19 +13,23 @@ socket.on("disconnect", function () {
 })
 
 socket.on("newMessage", function (message) {
-    console.log("New message!", message)
+    // console.log("New message!", message)
+    var momentMessage = moment(moment.createdAt).format("h:mm:ss a")
+
     var li = jQuery("<li></li>");
-    li.text(`${message.from} : ${message.text}`);
+    li.text(`${message.from} ${momentMessage}: ${message.text}`);
 
     jQuery("#messages").append(li);
 
 })
 
 socket.on("newLocationMessage", function (message) {
+    var momentMessage = moment(moment.createdAt).format("h:mm:ss a")
+
     var li = jQuery("<li></li>");
     var a = jQuery("<a target='_blank'>My current location</a>");
 
-    li.text(`${message.from} :  `);
+    li.text(`${message.from} ${momentMessage}:  `);
     a.attr("href", message.url);
     li.append(a);
     jQuery("#messages").append(li);
--- server/utils/message.js
+++ server/utils/message.js
@@ -1,17 +1,18 @@
+const moment = require("moment")
+
 var generateMessage = function(from, text) {
     return {
         from,
         text,
-        createdAt: new Date().getTime()
-    }
-}
+        createdAt: moment().valueOf()
+}}
 
 var generateLocationMessage = function (from, latitude, longitude) {
     return {
         from,
         url : `https://www.google.com/maps?q=${latitude},${longitude}`,
-        createdAt: new Date().getTime()
+        createdAt: moment().valueOf()
     }
 }
 
-module.exports = {generateMessage, generateLocationMessage}
+module.exports = {generateMessage, generateLocationMessage};
