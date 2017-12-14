edaf10efdd161fb0587b6e297e543dace08cbde3
spoonman <Chenfmmu@163.com>
2017-12-14 16:19:16 +0800

    Style the chat app

--- public/js/index.js
+++ public/js/index.js
@@ -31,28 +31,35 @@ socket.on("newLocationMessage", function (message) {
     jQuery("#messages").append(li);
 })
 
+var messageTextbox = jQuery("[name=message]");
+
 jQuery("#message-form").on("submit", function(e) {
     e.preventDefault();
     socket.emit("createMessage", {
         from : "User",
-        text : jQuery("[name=message]").val()
+        text : messageTextbox.val()
     }, function() {
-
+       messageTextbox.val("")
     });
 });
 
-var locationButton = jQuery("#SendLocation");
+var locationButton = jQuery("#send-location");
 locationButton.on("click", function() {
     if (!navigator.geolocation) {
         return alert("Geolocation not supported by your browser")
     }
 
+    locationButton.attr("disabled", "disabled").text("Sending Location ...");
+
     navigator.geolocation.getCurrentPosition(function(position){
+        locationButton.removeAttr("disabled").text("Send Location");
+
         socket.emit("createLocationMessage", {
             latitude : position.coords.latitude,
             longitude : position.coords.longitude
         });
     }, function() {
-        alert("Unable to fetch location.")
+        locationButton.removeAttr("disabled").text("Send Location");
+        alert("Unable to fetch location.");
     })
 })
